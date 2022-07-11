7699b26d29f6276b5856c2aee03e6cde81b34cbe
Raphael Andrews <raphaelandrews95@gmail.com>
2022-07-11 16:44:58 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
   return (
-    <h1>Oi</h1>
+    <>
+      <h1>Oi</h1>
+      <NotificationButton />
+    </>
   )
 }
 
