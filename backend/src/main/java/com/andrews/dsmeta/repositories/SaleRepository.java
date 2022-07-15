9dad5533d911185945d4a4634765f617fa175022
Raphael Andrews <raphaelandrews95@gmail.com>
2022-07-14 21:13:14 -0300

    API test

---
+++ backend/src/main/java/com/andrews/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.andrews.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.andrews.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
